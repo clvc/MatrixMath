eddd0ba47ac6cf22a4315ee939db59e5208e24a8
Neil Parikh <parikh.neil@me.com>
2014-12-01 01:24:29 -0500

    Elements should be an array of double*, not double

--- matrix.cpp
+++ matrix.cpp
@@ -8,7 +8,7 @@ Matrix::Matrix() {
 Matrix::Matrix(int r, int c) {
     rows = r;
     cols = c;
-    elements = (double **) malloc(rows * sizeof(double));
+    elements = (double **) malloc(rows * sizeof(double*));
     for (int i = 0; i < rows; i++) {
         elements[i] = (double *) malloc(cols * sizeof(double));
         for (int j = 0; j < cols; j++) {
