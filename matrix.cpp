2a579ff959391a4bd6a7df2e460f3851563d790d
Neil Parikh <parikh.neil@me.com>
2014-11-30 19:31:22 -0500

    Set up elements array

--- matrix.cpp
+++ matrix.cpp
@@ -1,4 +1,5 @@
 #include "matrix.h"
+#include <cstdlib>
 
 Matrix::Matrix() {
     // should we do stuff? nahhhhh
@@ -7,9 +8,20 @@ Matrix::Matrix() {
 Matrix::Matrix(int r, int c) {
     rows = r;
     cols = c;
+    elements = (double **) malloc(rows * sizeof(double));
+    for (int i = 0; i < rows; i++) {
+        elements[i] = (double *) malloc(cols * sizeof(double));
+        for (int j = 0; j < cols; j++) {
+            elements[i][j] = 0;
+        }
+    }
 }
 
 Matrix::~Matrix() {
+    for (int i = 0; i < rows; i++) {
+        free(elements[i]);
+    }
+    free(elements);
     // do stuff!
 }
 
