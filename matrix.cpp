6ccac6a7514998146f3dfa493d6e9158f20bcbd1
RowanFerrabee <rowan.ferrabee@gmail.com>
2014-12-09 01:03:39 -0500

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -195,4 +195,97 @@ double Matrix::getElement(int r, int c){
     return elements[r][c];
 }
 
+
+Matrix Matrix::cof() {
+    
+    if(rows == cols){
+        Matrix m(rows,cols);
+        Matrix deter(rows-1,cols-1);
+    
+        for (int i=0; i < m.rows; i = i+1) {
+            for (int j=0; j < m.cols; j = j+1) {
+                
+                int inc_x = 0, inc_y = 0;
+                for (int x=0; x<rows-1; x++) {
+                    for (int y=0; y<cols-1; y++) {
+                        if (x==i)
+                            inc_x = 1;
+                        if (y==j)
+                            inc_y = 1;
+                        deter.elements[x][y] = elements[x+inc_x][y+inc_y];
+                    } 
+                    inc_y = 0;
+                }
+
+                if((i+j)%2==0)
+                    m.elements[i][j] = deter.det();
+                else
+                    m.elements[i][j] = 0-deter.det();
+            }
+        }
+        cerr << "COFACTOR" << endl;
+        return m;
+    }
+    else {
+        cerr << "COFACTOR ERROR" << endl;
+        return Matrix(0,0);
+    }
+}
+
+Matrix Matrix::rref(){
+    Matrix m = (*this);
+    int pivotCol = 0;
+    int pivotRow = 0;
+    
+    while (pivotCol <= cols){
+        m.pivot(pivotRow,pivotCol);
+        pivotCol++;
+    }
+    
+    return m;
+}
+
+void Matrix::pivot(int &pivotRow, int pivotCol) {
+    int firstNonZero = -1;
+    for (int i=pivotRow; i<rows; i++) {
+        if (elements[i][pivotCol] != 0)
+            firstNonZero = i;
+    }
+    if (firstNonZero == -1)
+        return;
+    else {
+        if (firstNonZero != pivotRow) {
+            for (int j=pivotCol; j<cols; j++) {
+                double temp = elements[firstNonZero][j];
+                elements[firstNonZero][j] = elements[pivotRow][j];
+                elements[pivotRow][j] = temp;
+            }
+        }
+        for (int j = cols-1; j >= pivotCol; j--) {
+            elements[pivotRow][j] /= elements[pivotRow][pivotCol];
+        }
+        if (pivotRow<rows-1) {
+            for (int k = pivotRow+1; k < rows; k++) {
+                for (int j = cols-1; j >= pivotCol; j--) {
+                    elements[k][j] = elements[k][j] - elements[k][pivotCol]*elements[pivotRow][j];
+                }
+            }
+        }
+        if (pivotRow > 0) {
+            for (int k = pivotRow-1; k >= 0; k--) {
+                for (int j = cols-1; j >= pivotCol; j--) {
+                     elements[k][j] = elements[k][j] - elements[k][pivotCol]*elements[pivotRow][j];
+                }
+            }
+        }
+        pivotRow++;
+        return;
+    }
+}
+
+Matrix Matrix::inv() {
+    Matrix m = (*this);
+    return m.cof().transpose()*(1.0/m.det());
+}
+
 // continue on
